e5e62772860236fe65de901f639b3dd139c1b509
illustratumq <p.mochuk-me23@lll.kpi.ua>
2023-05-13 18:47:31 +0300

    add start_cmd text

--- app/handlers/private/start.py
+++ app/handlers/private/start.py
@@ -2,13 +2,19 @@ from aiogram import Dispatcher
 from aiogram.dispatcher.filters import CommandStart
 from aiogram.types import Message
 
-from app.database.services.repos import UserRepo
 from app.keyboards.inline.menu import menu_kb
 
 
 async def start_cmd(msg: Message):
     text = (
-        ''
+        '📚 Мої предмети\n'
+        'Додати або редагувати предмети\n\n'
+        '📒 Мої завдання\n'
+        'Додати або редагувати завдання для предметів\n\n'
+        '⚙ Налаштування\n'
+        'Вимкнути або увікнути сповіщення про дедлайни\n\n'
+        '💭 Питання\n'
+        'Гайд, як користуватися ботом'
     )
     await msg.answer(text, reply_markup=menu_kb())
 
